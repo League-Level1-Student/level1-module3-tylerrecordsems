d780d32bf47d3a96693e397ac5964b24b3ccd7c1
student <student@ip-172-31-56-145.us-west-2.compute.internal>
2021-01-18 13:16:33 -0800

    sup

--- src/_02_backpack/BackpackRunner.java
+++ src/_02_backpack/BackpackRunner.java
@@ -3,5 +3,10 @@ package _02_backpack;
 public class BackpackRunner {
 	public static void main(String[] args) {
 		new Backpack().packAndCheck();
+	Backpack blue = new Backpack();
+	blue.putInBackpack(new Pencil());
+	blue.putInBackpack(new Ruler());
+	blue.putInBackpack(new Textbook());
+	blue.goToSchool();
 	}
 }
